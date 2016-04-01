5b749450eb398a6ef42439cf4ee79842f84c0109
Dana Oira <danaoira@live.com>
2016-04-01 14:45:30 -0700

    Optimized user input error check; Fixed error in input file

--- cgv.py
+++ cgv.py
@@ -71,44 +71,14 @@ class Track:
 		self.add_course(prereq)
 		self.add_course(coreq)
 
-w = "# Major Prerequisite Tree"
+w = '# Major Prerequisite Tree'
 x = '"CPSC 120" -> "CPSC 121" [priority]'
 y = '"PHYS 225" -- "PHYS 225L"'
 
 re_courses = re.compile('\w+\s\d+\w*')
 
 cgv = Curriculum()
-mpt = Track("Major Prerequisite Tree")
-
-# mpt.add_prereq("CPSC 120", "CPSC 121")
-# mpt.add_prereq("CPSC 121", "CPSC 131")
-# mpt.add_prereq("CPSC 240", "CPSC 440")
-# mpt.add_prereq("CPSC 254", "CPSC 301")
-# mpt.add_prereq("CPSC 254", "CPSC 351")
-# mpt.add_prereq("CPSC 301", "CPSC 323")
-# mpt.add_prereq("CPSC 301", "CPSC 335")
-# mpt.add_prereq("CPSC 301", "CPSC 362")
-# mpt.add_prereq("CPSC 311", "CPSC 315")
-# mpt.add_prereq("CPSC 311", "CPSC 362")
-# mpt.add_prereq("CPSC 335", "CPSC 481")
-# mpt.add_prereq("CPSC 351", "CPSC 471")
-# mpt.add_prereq("ENGL 101", "CPSC 311")
-# mpt.add_prereq("CPSC 131", "CPSC 240")
-# mpt.add_prereq("CPSC 131", "CPSC 254")
-# mpt.add_prereq("CPSC 131", "CPSC 311")
-# mpt.add_prereq("CPSC 131", "CPSC 332")
-# mpt.add_prereq("MATH 150A", "MATH 150B")
-# mpt.add_prereq("MATH 150B", "MATH 338")
-# mpt.add_prereq("MATH 270A", "CPSC 240")
-# mpt.add_prereq("MATH 270A", "MATH 270B")
-# mpt.add_prereq("MATH 270B", "CPSC 335")
-# mpt.add_prereq("MATH 338", "CPSC 335")
-# print("track_name = " + core.name)
-# print("courses = " + str(core.courses))
-# print("prereq = " + str(core.prereq))
-
-for i in all_courses:
-	print(i)
+mpt = Track('Major Prerequisite Tree')
 
 # file parser
 for line in f:
@@ -128,40 +98,34 @@ for line in f:
 	# else:
 	# 	print(line, end='')
 
-print('\nAll courses in ' + str(mpt.name).upper() + ':\n')
+print('\nAll courses in ' + str(mpt.name).upper() + ':')
 
 # print list of courses
-for i, item in enumerate(mpt.courses, start=0):
+for i, item in enumerate(sorted(mpt.courses), start=0):
 	if i % 7 == 0:
 		print()
 	else:
 		print('{0:10s}'.format(item), end=' ')
-	# if len(course_list) is not 6:
-	# 	course_list.append(i)
-	# else:
-	# 	print('{0:10s} {1:10s} {2:10s} {3:10s} {4:10s} {5:10s}'.format(course_list[0], course_list[1], course_list[2], course_list[3], course_list[4], course_list[5]))
-		# course_list = []
 
-print('\nInput courses completed, type X to finish:\n')
+print('\n\nInput courses completed, type X to finish:\n')
 
+# handles course completed input
 def input_match(course):
-	if course in mpt.courses:
-		cgv.add_completed(course)
-	elif course == 'X':
-		pass
-	else:
-		print("Error")
+	for i in course:
+		if i in cgv.completed:
+			print('> repeat info: ' + str(i))
+		elif i in mpt.courses:
+			cgv.add_completed(i)
+		else:
+			print('> does not exist: ' + str(i))
 
 # course completed input & error check
-course_completed = input().upper()
-input_match(course_completed)
-while not course_completed == "X":
-	course_completed = input().upper()
+course_completed = re_courses.findall(input().upper())
+while len(course_completed) > 0:
 	input_match(course_completed)
-print(cgv.completed)
-	
+	course_completed = re_courses.findall(input().upper())
 
-
+print('Completed Courses: ' + str(cgv.completed))
 
 # convert to python graphviz
 
