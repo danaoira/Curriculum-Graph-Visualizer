b1a4a32a3c1c09214e7a9acc137ad417c6787be3
Dana Oira <danaoira@live.com>
2016-05-10 12:57:53 -0700

    Optimized tree population with hash map OrderedDict

--- cgt.py
+++ cgt.py
@@ -1,5 +1,6 @@
 # cgt.py - Curriculum Graph Tree - Dana Toribio
 
+import collections
 import os
 import re
 import sys
@@ -7,19 +8,15 @@ import sys
 class Node:
 	n = 0
 	def __init__(self, data):
-		Node.n += 1
-		self.n = Node.n
 		self.data = data
 		self.priority = False
 		self.units = 0
 		self.children = []
 
-	# def create_node(self, )
-
 	def add_child(self, obj):
 		self.children.append(obj)
 
-	def priority(self):
+	def set_priority(self):
 		self.priority = True
 
 	def get_child(self):
@@ -31,73 +28,44 @@ class Node:
 		elif 'MATH' in self:
 			self.units = 4
 
-z = Node("ROOT")
-
 re_courses = re.compile('\w+\s\d+\w*')	# regex for courses
 
+node_hash = collections.OrderedDict()
+node_hash['ROOT'] = Node("ROOT")
+
 f = open('studyplan.txt', 'r')
-node_hash = {}
 
-courses = list(set(re_courses.findall(f.read())))
-# courses.sort()
-# print(courses)
-node = (len(courses)+1) * [0]
-
-node[0] = Node('ROOT')
-node_hash['ROOT'] = node[0]
-
-# node[i] = courses[i-1]
-# print('node[' + str(i) + '] = ' + str(node[i]))
-f.seek(0)
-n = 1
+# populate study plan tree
 for line in f:
 	r = re_courses.findall(line)
 	if r and r[0] not in node_hash:
-		node[n] = Node(r[0])
-		node[0].add_child(node[n])
-		node_hash[r[0]] = node[n]
-		n += 1
+		node_hash[r[0]] = Node(r[0])					# create Node obj
+		node_hash['ROOT'].add_child(node_hash[r[0]])	# set as child to ROOT
+		if '*' in line:
+			node_hash[r[0]].set_priority()				# set priority
 	try:
 		if r and r[1] not in node_hash:
-			node[n] = Node(r[1])
-			node_hash[r[0]].add_child(node[n])
-			node_hash[r[1]] = node[n]
-			n += 1
+			node_hash[r[1]] = Node(r[1])
+			node_hash[r[0]].add_child(node_hash[r[1]])
+			if '*' in line:
+				node_hash[r[1]].set_priority()
 	except:
 		pass
 
-# for i in node_hash['ROOT'].children:
-# 	print(i.data)
-
-for i in range(len(node)):
-	print(node[i].data, end=', ')
-
-print()
-
-for i in range(len(node)):
-	if len(node[i].children) > 0:
-		print(str(node[i].data) + ' -> ', end='')
+for i in node_hash:
+	if len(node_hash[i].children) > 0:
+		if node_hash[i].priority == True:
+			print('*', end=' ')
+		print(str(node_hash[i].data) + ' -> ', end='')
 	else:
-		print(str(node[i].data))
-	for j in node[i].children:
-		if j != node[i].children[-1]:
+		print(node_hash[i].data)
+	for j in node_hash[i].children:
+		if j != node_hash[i].children[-1]:
 			print(j.data, end=', ')
 		else:
 			print(j.data)
 
+# BREADTH FIRST SEARCH
 
-# TEST: to view all node[i] values
-# for i in range(len(node)):
-# 	print('node[' + str(i) + '] = ' + str(node[i].data))
-
-	# if re[0] not in tree from DFS
-		# if  re_courses.findall(line)[0] not in node_hash:
-			# node_hash[re_courses.findall(line)[0]] = 
-		# create re[0] node
-		# set re[0] as child to ROOT
-		# if * in line -> priority = True
-		# set_units()
-	# if re[1] not in tree
-		# create re[1] node
-		# set re[1] as child of re[0]
-		# if * in line -> priority = True
+# def bfs(root_node):
+# 	return null
