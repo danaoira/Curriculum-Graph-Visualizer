7ac71aeed0931df86a9631652a7d17290fdafddb
Dana Oira <danaoira@live.com>
2016-05-11 11:34:37 -0700

    Organized tree population into functions

--- cgt.py
+++ cgt.py
@@ -6,7 +6,6 @@ import re
 import sys
 
 class Node:
-	n = 0
 	def __init__(self, data):
 		self.data = data
 		self.priority = False
@@ -28,6 +27,39 @@ class Node:
 		elif 'MATH' in self:
 			self.units = 4
 
+# populate study plan tree
+def populate_tree(file, tree):
+	for line in file:
+		r = re_courses.findall(line)
+		if r and r[0] not in tree:
+			tree[r[0]] = Node(r[0])				# create Node obj
+			tree['ROOT'].add_child(tree[r[0]])	# set as child to ROOT
+			if '*' in line:
+				tree[r[0]].set_priority()		# set priority
+		try:
+			if r and r[1] not in tree:
+				tree[r[1]] = Node(r[1])
+				tree[r[0]].add_child(tree[r[1]])
+				if '*' in line:
+					tree[r[1]].set_priority()
+		except:
+			pass
+
+# print tree results
+def print_tree(tree):
+	for i in tree:
+		if len(tree[i].children) > 0:
+			if tree[i].priority == True:
+				print('*', end=' ')
+			print(str(tree[i].data) + ' -> ', end='')
+		else:
+			print(tree[i].data)
+		for j in tree[i].children:
+			if j != tree[i].children[-1]:
+				print(j.data, end=', ')
+			else:
+				print(j.data)
+
 re_courses = re.compile('\w+\s\d+\w*')	# regex for courses
 
 node_hash = collections.OrderedDict()
@@ -35,35 +67,8 @@ node_hash['ROOT'] = Node("ROOT")
 
 f = open('studyplan.txt', 'r')
 
-# populate study plan tree
-for line in f:
-	r = re_courses.findall(line)
-	if r and r[0] not in node_hash:
-		node_hash[r[0]] = Node(r[0])					# create Node obj
-		node_hash['ROOT'].add_child(node_hash[r[0]])	# set as child to ROOT
-		if '*' in line:
-			node_hash[r[0]].set_priority()				# set priority
-	try:
-		if r and r[1] not in node_hash:
-			node_hash[r[1]] = Node(r[1])
-			node_hash[r[0]].add_child(node_hash[r[1]])
-			if '*' in line:
-				node_hash[r[1]].set_priority()
-	except:
-		pass
-
-for i in node_hash:
-	if len(node_hash[i].children) > 0:
-		if node_hash[i].priority == True:
-			print('*', end=' ')
-		print(str(node_hash[i].data) + ' -> ', end='')
-	else:
-		print(node_hash[i].data)
-	for j in node_hash[i].children:
-		if j != node_hash[i].children[-1]:
-			print(j.data, end=', ')
-		else:
-			print(j.data)
+populate_tree(f, node_hash)
+print_tree(node_hash)
 
 # BREADTH FIRST SEARCH
 
